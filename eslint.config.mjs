47b2498eac3764a6a92d34019d7939b9d1fa8f66
Padillahirpan <padillahirpan8@gmail.com>
2025-11-05 12:24:16 +0700

    init base

---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
+]);
+
+eslintConfig.push({
+  files: ["**/*.ts", "**/*.tsx"],
+  rules: {
+    // Example custom rule:
+    "react-hooks/exhautive-deps": "off",
+    "react/jsx-key": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+  },
+});
+
+export default eslintConfig;
